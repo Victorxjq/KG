26470bad5569715323d26576f1d841c2393febf0
kdd_xijunquan <xijunquan@gmail.com>
2018-12-07 15:21:54 +0800

    modify script

--- Count_doc_withwords.py
+++ Count_doc_withwords.py
@@ -5,7 +5,7 @@ from pyspark.mllib.feature import Word2Vec
 import jieba
 import re
 
-#test words
+#delete words
 # os.environ['PYSPARK_SUBMIT_ARGS'] = "--master mymaster --total-executor 2 --conf "
 def merge(list):
     result = []
